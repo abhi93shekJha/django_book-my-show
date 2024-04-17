e280b84043f7d78cd6fdf3a49aa7fd77942ba75f
abhishek.jha <abhishek.jha@maxerience.com>
2024-04-18 00:11:19 +0530

    settings.py modification

--- book_my_show/book_my_show/settings.py
+++ book_my_show/book_my_show/settings.py
@@ -146,7 +146,7 @@ STATIC_ROOT = '/vol/web/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
 
